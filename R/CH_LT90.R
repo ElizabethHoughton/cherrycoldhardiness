58b5e66dc6344e5eadc755e457c110cb8486ee66
Elizabeth Houghton <Elizabeth@Elizabeths-MBP.hitronhub.home>
2021-08-02 13:44:56 -0700

    Change Model files to .Rda

--- R/CH_LT90.R
+++ R/CH_LT90.R
@@ -17,11 +17,7 @@
 #' @export
 CH_LT90 <- function(Calculated_CU_FU90=NULL){
   # load in the fitted Model10, Model50, Model90
-<<<<<<< HEAD
   load("data/Model90.Rda") # vs. readRDS("data/Model90.rds") which doesnt seem to be recognized
-=======
-  load("data/Model90.rda") # vs. readRDS("data/Model90.rds") which doesnt seem to be recognized
->>>>>>> 9de035b09986d76803bb7298d7e45325e437e853
   # predict the LT90 values based on file upload (inputID labeled 'csv_input')
   PredictLT90 <- as.data.frame(AICcmodavg::predictSE.gls(Model90, newdata = Calculated_CU_FU90, se= TRUE))
   # Calculate confidence intervals and create data frames out of them
